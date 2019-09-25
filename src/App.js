0d78c956a5d175aeb994dc34a392a2d1c52554a2
matheswaaran <matheswaarans@gmail.com>
2019-09-25 15:01:07 +0530

    Added button for Like

--- src/App.js
+++ src/App.js
@@ -6,21 +6,38 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      playList: [
+        {
+          file: "https://content.jwplatform.com/manifests/yp34SRmf.m3u8",
+          image: "https://1kabswnt2ua3ivl0cuqv2f17-wpengine.netdna-ssl.com/wp-content/uploads/2014/10/Bike-riding.jpg",
+        }
+      ],
     };
   }
 
+  setupPlayerOnReady = (event) => {
+    window.jwplayer('live-player').addButton(
+      "./assests/like.png",
+      "Like the Video",
+      () => {
+        console.log("video liked");
+      },
+      "like-video"
+    );
+  };
+
   render() {
     return (
-      <React.Fragment>
-        <div style={{ height: 297, width: 528 }}>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+        <div style={{ height: 720, width: 1280 }}>
           <ReactJWPlayer
             playerId="live-player"
-            file="https://wowzaprod211-i.akamaihd.net/hls/live/859782/f52e9441/playlist.m3u8"
-            playerScript='https://cdn.jwplayer.com/libraries/pUQtdZir.js'
+            onReady={this.setupPlayerOnReady}
+            playlist={this.state.playList}
+            playerScript="https://cdn.jwplayer.com/libraries/pUQtdZir.js"
           />
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
